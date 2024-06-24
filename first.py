67cb42777377ff266134b575c515eaef88046083
Алексей <blv02495@gmail.com>
2024-06-24 14:25:29 +0300

    first

---
+++ first.py
@@ -0,0 +1,25 @@
+a = "String"
+print(len(a))
+
+first = 5
+second = 10
+summa = first + second
+print(summa)
+diff = second - first
+print(diff)
+
+first = 43
+second = 11
+third = 59
+mean = first+second+third / 3
+print(mean)
+
+first_string = "Вторник"
+second_string = "Понедельник,"
+print(second_string, first_string)
+
+a = 2
+b = 4
+c = 6
+f = (a*b)+(a*c)**3/2
+print(f)
